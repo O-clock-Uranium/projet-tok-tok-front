faa80bc8fb3dedc2b3c5e38227acb1e5aa9ef7ab
Tom-Esteban <esteban.tom26@gmail.com>
2023-07-13 01:00:37 +0200

    Configuration Router, mise en place des premières routes, nouveaux composants

---
+++ src/components/Error/Error.tsx
@@ -0,0 +1,5 @@
+function Error() {
+  return <p>Nous sommes désolé, Une erreur s&apos;est produite.</p>;
+}
+
+export default Error;
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,36 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
+
+import store from './store';
 
 import App from './components/App/App';
+import Error from './components/Error/Error';
+import Favorites from './components/Favorites/Favorites';
 
 import './styles/index.scss';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} errorElement={<Error />}>
+      <Route errorElement={<Error />}>
+        <Route path="/favoris" element={<Favorites />} />
+      </Route>
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
 );
