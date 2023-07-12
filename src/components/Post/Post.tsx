ed60cfd18429abadcc30215a2f19826ecbe8d8dc
JonathanBuignet <jonathan.buignet@gmail.com>
2023-07-13 01:08:32 +0200

    création des composants Post et Posts

---
+++ src/components/Post/Post.tsx
@@ -0,0 +1,7 @@
+import './Post.scss';
+
+function Post() {
+  return <div className="post">un post</div>;
+}
+
+export default Post;
---
+++ src/components/Posts/Posts.tsx
@@ -0,0 +1,14 @@
+import Post from '../Post/Post';
+import './Posts.scss';
+
+function Posts() {
+  return (
+    <div className="posts">
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
+}
+
+export default Posts;
