b62527f8f03598b2c72c4490264eb6afbf94bcf5
JonathanBuignet <jonathan.buignet@gmail.com>
2023-07-13 12:06:38 +0200

    Mise en place de material UI pour les publications / en cours

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,7 +1,12 @@
+import OneCard from './OneCard';
 import './Post.scss';
 
 function Post() {
-  return <div className="post">un post</div>;
+  return (
+    <div className="post">
+      <OneCard />
+    </div>
+  );
 }
 
 export default Post;
--- src/components/Posts/Posts.tsx
+++ src/components/Posts/Posts.tsx
@@ -1,12 +1,22 @@
+import Stack from '@mui/material/Stack';
+// import { Stack } from '@mui/system'; // ne sait pas lequel des deux est le mieux
 import Post from '../Post/Post';
+
 import './Posts.scss';
 
 function Posts() {
   return (
     <div className="posts">
-      <Post />
-      <Post />
-      <Post />
+      <Stack
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        <Post />
+        <Post />
+        <Post />
+      </Stack>
     </div>
   );
 }
