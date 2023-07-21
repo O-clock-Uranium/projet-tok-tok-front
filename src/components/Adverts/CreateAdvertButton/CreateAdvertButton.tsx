f240ffc89af790cbab43b247e9ead6dc17285682
JonathanBuignet <jonathan.buignet@gmail.com>
2023-07-21 14:38:31 +0200

    Modal sans config

--- src/components/Adverts/CreateAdvertButton/CreateAdvertButton.tsx
+++ src/components/Adverts/CreateAdvertButton/CreateAdvertButton.tsx
@@ -1,26 +1,65 @@
+import { Modal, Paper, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
-import * as React from 'react';
+import { Box } from '@mui/system';
+import { useState } from 'react';
 
 export default function CreateAdvertButton() {
+  const [Open, setOpen] = useState(false);
+
+  const handleClick = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
   return (
-    <Button
-      variant="contained"
-      sx={{
-        backgroundColor: 'primary.dark',
-        color: '#fff',
-        textTransform: 'none',
-        fontSize: '1.5rem',
-        borderRadius: '2rem',
-        fontFamily: 'DM Sans',
-        fontStyle: 'normal',
-        fontWeight: '500',
-        lineHeight: 'normal',
-        p: '2rem',
-        height: '6rem',
-        boxShadow: 0,
-      }}
-    >
-      Créer une Annonce
-    </Button>
+    <>
+      <Button
+        variant="contained"
+        onClick={() => setOpen(!Open)}
+        sx={{
+          backgroundColor: 'primary.dark',
+          color: '#fff',
+          textTransform: 'none',
+          fontSize: '1.5rem',
+          borderRadius: '2rem',
+          fontFamily: 'DM Sans',
+          fontStyle: 'normal',
+          fontWeight: '500',
+          lineHeight: 'normal',
+          p: '2rem',
+          height: '6rem',
+          boxShadow: 0,
+        }}
+      >
+        Créer une Annonce
+      </Button>
+      {/* <AdvertModal /> */}
+      <Modal
+        open={Open}
+        onClose={handleClose}
+        aria-labelledby="modal-advert-form"
+        aria-describedby="modal-advert-form"
+      >
+        <Paper
+          sx={{
+            backgroundColor: 'white',
+            margin: 'auto',
+            mt: '20vh',
+            width: '50%',
+            height: '50%',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            blablabla
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Paper>
+      </Modal>
+    </>
   );
 }
---
+++ src/components/Modals/AdvertModal.tsx
@@ -0,0 +1,27 @@
+// import { Box, Modal, Typography } from '@mui/material';
+// import React from 'react';
+
+// function AdvertModal() {
+//   const ModalAdvert = () => {
+//     const [Open, setOpen] = React.useState(false);
+//   };
+//   return (
+//     <Modal
+//       open={open}
+//       onClose={handleClose}
+//       aria-labelledby="modal-advert-form"
+//       aria-describedby="modal-advert-form"
+//     >
+//       <Box>
+//         <Typography id="modal-modal-title" variant="h6" component="h2">
+//           Text in a modal
+//         </Typography>
+//         <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+//           Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+//         </Typography>
+//       </Box>
+//     </Modal>
+//   );
+// }
+
+// export default AdvertModal;
