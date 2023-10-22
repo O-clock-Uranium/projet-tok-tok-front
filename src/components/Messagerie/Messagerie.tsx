16760aaa17d4ed7dac0ad1292f0f96da26abf117
Jonathan Buignet <jonathan.buignet@gmail.com>
2023-10-23 01:54:41 +0200

    ajout messagerie

--- src/components/Messagerie/Messagerie.tsx
+++ src/components/Messagerie/Messagerie.tsx
@@ -13,106 +13,98 @@ export default function Messagerie() {
   const dispatch = useAppDispatch();
   const contacts = useAppSelector((state) => state.messagerie.contacts);
 
-  // const contactId = contacts.map((contact) => contact.id);
-  // console.log(contactId);
-
-  // 0:2
-  // 1:3
-
   useEffect(() => {
     dispatch(fetchContacts());
   }, [dispatch]);
 
   return (
-    <Box
-      sx={{
-        height: '80vh',
-        width: '100rem',
-        position: 'relative',
-        top: '15rem',
-        margin: 'auto',
-      }}
-    >
-      {contacts.length > 0 ? (
-        <Stack direction="row">
-          <MenuContact
-            contacts={contacts}
-            destinataireId={destinataireId}
-            setDestinataireId={setDestinataireId}
-            destinataireName={destinataireName}
-            setDestinataireName={setDestinataireName}
-          />
-          <Stack>
-            <div className="chat">
-              {/* barre du haut */}
-              <Stack
-                direction="row"
-                sx={{
-                  backgroundColor: '#fff',
-                  p: '1rem',
-                  mb: '0.5rem',
-                  pl: '2rem',
-                  borderRadius: '1rem',
-                }}
-              >
-                <Typography
-                  sx={{
-                    color: 'primary.dark',
-                    height: '4rem',
-                    display: 'flex',
-                    alignItems: 'center',
-                    fontSize: '2rem',
-                    fontFamily: 'DM Sans',
-                  }}
-                >
-                  {destinataireName}
-                </Typography>
-              </Stack>
-              {/* Bloc */}
-              <Paper
-                elevation={0}
-                sx={{
-                  width: '80rem',
-                  // display: 'flex',
-                  position: 'relative',
-                  p: '2rem',
-                  borderRadius: '1rem',
-                  backgroundColor: '#fff',
-                  height: '60rem',
-                  overflowY: 'auto',
-                  '&::-webkit-scrollbar': {
-                    width: '1rem',
-                    backgroundColor: '#F5F5F5',
-                  },
-                  '&::-webkit-scrollbar-track': {
-                    '&::-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.1)',
-                    backgroundColor: '#49c1ad',
-                    borderRadius: '10px',
-                  },
-                  '&::-webkit-scrollbar-thumb': {
-                    backgroundColor: '#03665C',
-                    borderRadius: '25px',
-                  },
-                }}
-              >
-                <Messages />
+    <>
+      <div className="messages-container">
+        {contacts.length > 0 ? (
+          <Stack direction="row">
+            <MenuContact
+              contacts={contacts}
+              destinataireId={destinataireId}
+              setDestinataireId={setDestinataireId}
+              destinataireName={destinataireName}
+              setDestinataireName={setDestinataireName}
+            />
+            <Stack>
+              <div className="chat">
+                {/* barre du haut */}
                 <Stack
                   direction="row"
-                  alignItems="flex-end"
-                  justifyContent="flex-end"
+                  sx={{
+                    backgroundColor: '#fff',
+                    p: '1rem',
+                    mb: '0.5rem',
+                    pl: '2rem',
+                    borderRadius: '1rem',
+                  }}
                 >
-                  <Form destinataireId={destinataireId} />
+                  <Typography
+                    sx={{
+                      color: 'primary.dark',
+                      height: '4rem',
+                      display: 'flex',
+                      alignItems: 'center',
+                      fontSize: '2rem',
+                      fontFamily: 'DM Sans',
+                    }}
+                  >
+                    {destinataireName}
+                  </Typography>
                 </Stack>
-              </Paper>
-            </div>
+                {/* Bloc */}
+                <Paper
+                  elevation={0}
+                  sx={{
+                    width: '80rem',
+                    display: 'flex',
+                    position: 'relative',
+                    p: '2rem',
+                    borderRadius: '1rem',
+                    backgroundColor: '#fff',
+                    height: '60rem',
+                    overflowY: 'auto',
+                    '&::-webkit-scrollbar': {
+                      width: '1rem',
+                      backgroundColor: '#F5F5F5',
+                    },
+                    '&::-webkit-scrollbar-track': {
+                      '&::-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.1)',
+                      backgroundColor: '#49c1ad',
+                      borderRadius: '10px',
+                    },
+                    '&::-webkit-scrollbar-thumb': {
+                      backgroundColor: '#03665C',
+                      borderRadius: '25px',
+                    },
+                  }}
+                >
+                  <Messages />
+                  <Stack
+                    direction="row"
+                    alignItems="flex-end"
+                    justifyContent="flex-end"
+                  >
+                    <Form destinataireId={destinataireId} />
+                  </Stack>
+                </Paper>
+              </div>
+            </Stack>
           </Stack>
-        </Stack>
-      ) : (
-        <Box
-          width="100rem"
-          sx={{ backgroundColor: '#fff', borderRadius: '2rem' }}
-        >
-          <Stack direction="column" padding="3rem">
+        ) : (
+          /* Composant quand pas de messages ------------------------*/
+          <Box
+            width="80%"
+            sx={{
+              backgroundColor: '#fff',
+              borderRadius: '2rem',
+              margin: 'auto',
+              padding: '2rem',
+            }}
+          >
             <Typography
               sx={{
                 fontSize: '1.8rem',
@@ -127,9 +119,116 @@ export default function Messagerie() {
               Cliquez sur le bouton "contacter le vendeur" dans une annonce pour
               que vos différentes conversations s&apos;affichent ici.
             </Typography>
-          </Stack>
-        </Box>
-      )}
-    </Box>
+          </Box>
+        )}
+      </div>
+    </>
+    // <Box
+    //   sx={{
+    //     height: '80vh',
+    //     width: '100rem',
+    //     position: 'relative',
+    //     top: '15rem',
+    //     margin: 'auto',
+    //   }}
+    // >
+    //   {contacts.length > 0 ? (
+    //     <Stack direction="row">
+    //       <MenuContact
+    //         contacts={contacts}
+    //         destinataireId={destinataireId}
+    //         setDestinataireId={setDestinataireId}
+    //         destinataireName={destinataireName}
+    //         setDestinataireName={setDestinataireName}
+    //       />
+    //       <Stack>
+    //         <div className="chat">
+    //           {/* barre du haut */}
+    //           <Stack
+    //             direction="row"
+    //             sx={{
+    //               backgroundColor: '#fff',
+    //               p: '1rem',
+    //               mb: '0.5rem',
+    //               pl: '2rem',
+    //               borderRadius: '1rem',
+    //             }}
+    //           >
+    //             <Typography
+    //               sx={{
+    //                 color: 'primary.dark',
+    //                 height: '4rem',
+    //                 display: 'flex',
+    //                 alignItems: 'center',
+    //                 fontSize: '2rem',
+    //                 fontFamily: 'DM Sans',
+    //               }}
+    //             >
+    //               {destinataireName}
+    //             </Typography>
+    //           </Stack>
+    //           {/* Bloc */}
+    //           <Paper
+    //             elevation={0}
+    //             sx={{
+    //               width: '80rem',
+    //               // display: 'flex',
+    //               position: 'relative',
+    //               p: '2rem',
+    //               borderRadius: '1rem',
+    //               backgroundColor: '#fff',
+    //               height: '60rem',
+    //               overflowY: 'auto',
+    //               '&::-webkit-scrollbar': {
+    //                 width: '1rem',
+    //                 backgroundColor: '#F5F5F5',
+    //               },
+    //               '&::-webkit-scrollbar-track': {
+    //                 '&::-webkit-box-shadow': 'inset 0 0 6px rgba(0,0,0,0.1)',
+    //                 backgroundColor: '#49c1ad',
+    //                 borderRadius: '10px',
+    //               },
+    //               '&::-webkit-scrollbar-thumb': {
+    //                 backgroundColor: '#03665C',
+    //                 borderRadius: '25px',
+    //               },
+    //             }}
+    //           >
+    //             <Messages />
+    //             <Stack
+    //               direction="row"
+    //               alignItems="flex-end"
+    //               justifyContent="flex-end"
+    //             >
+    //               <Form destinataireId={destinataireId} />
+    //             </Stack>
+    //           </Paper>
+    //         </div>
+    //       </Stack>
+    //     </Stack>
+    //   ) : (
+    //     <Box
+    //       width="100rem"
+    //       sx={{ backgroundColor: '#fff', borderRadius: '2rem' }}
+    //     >
+    //       <Stack direction="column" padding="3rem">
+    //         <Typography
+    //           sx={{
+    //             fontSize: '1.8rem',
+    //             fontFamily: 'DM Sans',
+    //             fontStyle: 'normal',
+    //             fontWeight: '500',
+    //             lineHeight: 'normal',
+    //             p: '1rem',
+    //           }}
+    //         >
+    //           Vous navez pas encore de messages ? <br />
+    //           Cliquez sur le bouton "contacter le vendeur" dans une annonce pour
+    //           que vos différentes conversations s&apos;affichent ici.
+    //         </Typography>
+    //       </Stack>
+    //     </Box>
+    //   )}
+    // </Box>
   );
 }
